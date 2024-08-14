3f9a6b238cccf322fa97623a836f719cc50b2bcb
Sachin Gaikwad <sachingaikwadj2204@gmail.com>
2024-08-14 11:18:00 +0530

    Get All Student Method

--- StudentManagementSystem/src/main/java/com/cjc/main/controller/StudentController.java
+++ StudentManagementSystem/src/main/java/com/cjc/main/controller/StudentController.java
@@ -1,8 +1,14 @@
 package com.cjc.main.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cjc.main.model.Student;
 import com.cjc.main.servicei.StudentServiceI;
 
 @RestController
@@ -11,4 +17,13 @@ public class StudentController {
 	
 	@Autowired private StudentServiceI ssi;
 
+	
+	@GetMapping("/getAllStudent")
+	public ResponseEntity<List<Student>> getAllStudent(){
+		
+		List<Student> students = ssi.getAllStudent();
+		
+		return new ResponseEntity<List<Student>>(students,HttpStatus.OK);
+		
+	}
 }
--- StudentManagementSystem/src/main/java/com/cjc/main/servicei/StudentServiceI.java
+++ StudentManagementSystem/src/main/java/com/cjc/main/servicei/StudentServiceI.java
@@ -1,5 +1,11 @@
 package com.cjc.main.servicei;
 
+import java.util.List;
+
+import com.cjc.main.model.Student;
+
 public interface StudentServiceI {
+
+	List<Student> getAllStudent();
 
 }
