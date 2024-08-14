7fa03f341da0353c9af3c59062eac3196d4e6ee4
smssarode123 <sagarsarode1997@gmail.com>
2024-08-14 11:48:31 +0530

    By sagar

--- StudentManagementSystem/src/main/java/com/cjc/main/controller/StudentController.java
+++ StudentManagementSystem/src/main/java/com/cjc/main/controller/StudentController.java
@@ -1,8 +1,13 @@
 package com.cjc.main.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cjc.main.model.Student;
 import com.cjc.main.servicei.StudentServiceI;
 
 @RestController
@@ -10,5 +15,15 @@ public class StudentController {
 	
 	
 	@Autowired private StudentServiceI ssi;
+	
+	
+	@GetMapping("/getSinglestudent/{studentName}")
+	public ResponseEntity<Student> getSingleStudent(@PathVariable String studentName) 
+	{
+		Student student = ssi.getSingleStudent(studentName);
+		
+		
+		return new ResponseEntity<Student>(student, HttpStatus.OK);
+	}
 
 }
