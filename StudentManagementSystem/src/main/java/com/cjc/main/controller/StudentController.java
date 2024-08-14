54c5f07025d1cc4aa69aa91ca69af067ae089b5e
Lenovo <Lenovo@DESKTOP-TC9U27S>
2024-08-14 11:44:34 +0530

    BY MAHESH

--- StudentManagementSystem/src/main/java/com/cjc/main/controller/StudentController.java
+++ StudentManagementSystem/src/main/java/com/cjc/main/controller/StudentController.java
@@ -1,8 +1,15 @@
 package com.cjc.main.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cjc.main.model.Student;
 import com.cjc.main.servicei.StudentServiceI;
 
 @RestController
@@ -10,5 +17,15 @@ public class StudentController {
 	
 	
 	@Autowired private StudentServiceI ssi;
+	
+	
+	@PutMapping("/updateDetail/{studentRollNumber}")
+	public ResponseEntity<Student> updatedetail(@RequestBody Student stu,@PathVariable int studentRollNumber )
+	{
+		Student student=ssi.updatedetail(studentRollNumber, stu);
+		ResponseEntity<Student> restu=new ResponseEntity<Student>(student,HttpStatus.OK);
+		return restu;
+	}
+	
 
 }
--- StudentManagementSystem/src/main/java/com/cjc/main/serviceimpl/StudentServiceImpl.java
+++ StudentManagementSystem/src/main/java/com/cjc/main/serviceimpl/StudentServiceImpl.java
@@ -1,14 +1,31 @@
 package com.cjc.main.serviceimpl;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.cjc.main.model.Student;
 import com.cjc.main.repository.StudentRepository;
 import com.cjc.main.servicei.StudentServiceI;
 
 @Service
 public class StudentServiceImpl implements StudentServiceI{
 	
+	
 	@Autowired private StudentRepository reposiroty;
+
+	@Override
+	public Student updatedetail(int studentRollNumber, Student stu) {
+		Optional<Student> student =reposiroty.findById(studentRollNumber);
+		if(student.isPresent()) 
+		{
+			Student updatestu = student.get();
+			updatestu.setStudentRollNumber(studentRollNumber);
+			return reposiroty.save(stu);
+			
+		}
+		return null;
+	}
 
 }
